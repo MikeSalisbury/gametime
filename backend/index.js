c1015e7ea6996dd4d36f0e201f46477e274395ad
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-02 22:42:23 -0800

    add passport and strategies for oauth

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there' });
-});
+passport.use(new GoogleStrategy());
+passport.use(new FacebookStrategy());
 
 const PORT =  process.env.PORT || 5000;
 app.listen(PORT);
