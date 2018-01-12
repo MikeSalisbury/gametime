6b47505a3352ed27e25d8b4a9bca506f5f4b3c2b
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-12 00:40:12 -0800

    correct new user promise/done flow

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -12,21 +12,23 @@ passport.use(new GoogleStrategy({
   callbackURL: '/auth/google/callback'
  },
  (accessToken, refreshToken, profile, done) => {
-  User.findOne({ authId: profile.id }).then(existingUser => {
-    if (existingUser) {
-      return done(null, existingUser);
-    } else {
-      const user = new User({
-        authId: profile.id,
-        email: profile.emails[0].value,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName
-      }).save();
-      done(null, user);
-    }
-  });
- })
-);
+   User.findOne({ authId: profile.id }).then(existingUser => {
+
+     if (existingUser) {
+       return done(null, existingUser);
+     } else {
+      new User({
+       authId: profile.id,
+       email: profile.emails[0].value,
+       firstName: profile.name.givenName,
+       lastName: profile.name.familyName
+      }).save()
+      .then(user => done(null, user));
+     }
+     
+   });
+  })
+ );
 
 passport.use(new FacebookStrategy({
   clientID: keys.facebookAppId,
@@ -36,17 +38,19 @@ passport.use(new FacebookStrategy({
  },
  (accessToken, refreshToken, profile, done) => {
    User.findOne({ authId: profile.id }).then(existingUser => {
+
      if (existingUser) {
        return done(null, existingUser);
      } else {
-       const user = new User({
-         authId: profile.id,
-         email: profile.emails[0].value,
-         firstName: profile.name.givenName,
-         lastName: profile.name.familyName
-       }).save();
-       done(null, user);
+      new User({
+       authId: profile.id,
+       email: profile.emails[0].value,
+       firstName: profile.name.givenName,
+       lastName: profile.name.familyName
+      }).save()
+      .then(user => done(null, user));
      }
+
    });
   })
  );
