ce6b47bcb2ee991956e23a514ab514f25b72e6a0
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-12 12:33:54 -0800

    add logout route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -29,6 +29,12 @@ module.exports = app => {
   );
 
   app.get(
+    '/api/logout', (req, res) => {
+      req.logout();
+      res.redirect('/');
+    });
+
+  app.get(
     '/api/current_user', (req, res) => {
       res.send(req.user);
     });
