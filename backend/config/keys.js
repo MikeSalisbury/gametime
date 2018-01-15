acee30a28411cb5c81eeae6c43eb920eaad2d192
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-15 12:35:33 -0800

    fix bug with keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -2,7 +2,7 @@
 
 if (process.env.NODE_ENV === 'production') {
   // in production
-  module.exports = require('.prod');
+  module.exports = require('./prod');
 } else {
   // in dev
   module.exports = require('./dev');
