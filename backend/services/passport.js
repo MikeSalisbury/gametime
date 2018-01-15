8c3a83832849ff515c92b2626abbec3f235fdaa4
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-15 13:01:43 -0800

    update prod/dev oauth

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -19,7 +19,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'keys.googleURICallback'
  },
  (accessToken, refreshToken, profile, done) => {
    User.findOne({ authId: profile.id }).then(existingUser => {
