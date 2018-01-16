2bb4f1d90a200dbefa61943fd8110bcd4a537e3e
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-15 21:39:52 -0800

    tweak fb oauth callback

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -4,7 +4,7 @@ module.exports = {
   facebookAppId: process.env.FACEBOOK_APP_ID,
   facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
   googleURICallback: 'https://gametimeapp.herokuapp.com/auth/google/callback',
-  facebookURICallback: 'https://gametimeapp.herokuapp.com/auth/facebook/callback',
+  facebookURICallback: 'https://gametimeapp.herokuapp.com/auth/facebook/callback/',
   mongoURI: process.env.MONGO_URI,
   cookieKey1: process.env.COOKIE_KEY_1,
   cookieKey2: process.env.COOKIE_KEY_2,
