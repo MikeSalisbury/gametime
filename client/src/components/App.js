e7c99d4e4bdf32c745d7eb5d8227e1faa0842307
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-26 11:34:22 -0800

    create header and footer component skeletons refactor app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
-const Header = () => <h2>Header</h2>;
-const Footer = () => <h2>Footer</h2>;
+
 const Landing = () => <h2>Landing</h2>;
-const GameForm = () => <h2>Game Form</h2>;
-const GameShow = () => <h2>Game Show</h2>;
+const GamesNew = () => <h2>Game New Form</h2>;
+const GamesShow = () => <h2>Game Show</h2>;
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
       <div>
-        <Route path='/' component={Header} />
-        <Route path='/' component={Footer} />
+        <Header />
+        <Footer />
         <Switch>
           <Route exact path='/' component={Landing} />
-          <Route exact path ='/game/new' component={GameForm} />
-          <Route exact path ='/game/:gameId' component={GameShow} />
+          <Route exact path ='/games/new' component={GamesNew} />
+          <Route exact path ='/games/:gameId' component={GamesShow} />
         </Switch>
       </div>
       </BrowserRouter>
