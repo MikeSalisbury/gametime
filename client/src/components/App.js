74c85fd0c7a9e207e4763833450ede82df4d2122
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-26 10:28:35 -0800

    add BrowserRouter and skeleton routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Landing = () => <h2>Landing</h2>;
+const GameForm = () => <h2>Game Form</h2>;
+const GameShow = () => <h2>Game Show</h2>;
 
 const App = () => {
   return (
     <div>
-      Hi There!
+      <BrowserRouter>
+      <div>
+        <Route path='/' component={Header} />
+        <Route exact path='/' component={Landing} />
+      </div>
+      </BrowserRouter>
     </div>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
+import configureStore from './store/store';
 
 import App from './components/App';
 
-const root = document.getElementById('root');
 
-const store = createStore(rootReducer, {}, applyMiddleware() );
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, root);
+document.addEventListener("DOMContentLoaded", () => {
+  let store = configureStore();
+  
+  const root = document.getElementById('root');
+  ReactDOM.render(<Provider store={store}><App /></Provider>, root);
+});
