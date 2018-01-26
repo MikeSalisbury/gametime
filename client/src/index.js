ab487c9523ff6a7f706e9b8689263f5806e161cc
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-25 18:38:44 -0800

    add Provider and basic Store template

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
 import App from './components/App';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<App />, root);
+const store = createStore(() => [], {}, applyMiddleware() );
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, root);
