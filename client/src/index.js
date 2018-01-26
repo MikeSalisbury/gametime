c7cb472809761da684d555b2304e014cf385571a
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-25 20:07:15 -0800

    add authReducer and rootReducer setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
 
 import App from './components/App';
 
 const root = document.getElementById('root');
 
-const store = createStore(() => [], {}, applyMiddleware() );
+const store = createStore(rootReducer, {}, applyMiddleware() );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, root);
