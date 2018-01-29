aed02b6265f1d9918a9d7823365ae08c3ab6fb1d
Mike Salisbury <michael.salisbury87@gmail.com>
2018-01-29 04:18:55 -0800

    fetchUser functional, refactor App.js to call fetchUser at mount

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from './header/Header';
 import Footer from './footer/Footer';
-
+import * as actions from '../actions/index';
 
 const Landing = () => <h2>Landing</h2>;
 const GamesNew = () => <h2>Game New Form</h2>;
 const GamesShow = () => <h2>Game Show</h2>;
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-      <div>
-        <Header />
-        <Footer />
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route exact path ='/games/new' component={GamesNew} />
-          <Route exact path ='/games/:gameId' component={GamesShow} />
-        </Switch>
-      </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
 
-export default App;
+  componentDidMount(){
+    this.props.fetchUser();
+  }
+
+  render(){
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Footer />
+            <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route exact path ='/games/new' component={GamesNew} />
+            <Route exact path ='/games/:gameId' component={GamesShow} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,6 @@
 
 export default function(state = {}, action) {
+  console.log(action);
   switch (action.type) {
     default:
     return state;
