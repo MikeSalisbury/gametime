337a2526fd67d807cfc87ff503bd2b911af733bd
Mike Salisbury <michael.salisbury87@gmail.com>
2018-02-10 22:17:25 -0800

    fix bug with index.js breaking heroku push

--- index.js
+++ index.js
@@ -24,15 +24,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-require('./routes/gameRoutes')(app);
+// require('./routes/gameRoutes')(app);
 
-if (process.env.NODE_ENV === 'Production') {
+if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // Like main.js or main.css files
   app.use(express.static('client/build'));
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path');
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
