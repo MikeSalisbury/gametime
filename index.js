c2f809a85f8d68107da9c45ca9df63bbb78c4d83
Mike Salisbury <michael.salisbury87@gmail.com>
2018-02-10 22:11:43 -0800

    fixed packageJSON to properly deploy on Heroku

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/gameRoutes')(app);
+
+if (process.env.NODE_ENV === 'Production') {
+  // Express will serve up production assets
+  // Like main.js or main.css files
+  app.use(express.static('client/build'));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT =  process.env.PORT || 5000;
 app.listen(PORT);
