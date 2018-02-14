5bc5dd8a11fe663dc3e94b4ee12fb18ffbe7b92c
Mike Salisbury <michael.salisbury87@gmail.com>
2018-02-13 20:55:01 -0800

    fix bug with createGame action

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, CREATE_GAME } from './types';
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user')
