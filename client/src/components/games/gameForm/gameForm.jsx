5e34bb9e4f43f24900b5b26951c9d4f2427c8794
Mike Salisbury <michael.salisbury87@gmail.com>
2018-02-15 00:07:04 -0800

    start building/styling gameForm component

--- client/src/components/games/gameForm/gameForm.jsx
+++ client/src/components/games/gameForm/gameForm.jsx
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
+import './gameForm.css';
 
 class GameForm extends Component {
 
   constructor(props) {
     super(props);
+    this.state = { title: "", };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
 
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createGame(this.state).then(id => this.props.history.push(`/games/${id}`));
+  }
+
+  update(prop) {
+    return (e) => (
+      this.setState({[prop]: e.target.value})
+    );
+  }
+
+
   render() {
+    console.log(this.state.title);
+    const { title } = this.state;
+
     return(
       <div className='gameForm-wrapper'>
-        <h1>Game Form Component!</h1>
+        <form className='gameForm-form' onSubmit={this.handleSubmit}>
+          <label><div className='gameForm-label'>GAME TITLE</div>
+          <input
+            className='gameForm-gameTitle'
+            type='text'
+            placeholder='Give your game a short distinct name'
+            onChange={this.update('title')}
+            value={title}/><br/>
+          </label>
+        </form>
       </div>
     );
   }
