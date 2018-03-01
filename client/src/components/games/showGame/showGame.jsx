9b6f5c933947669622ec2b3945c384af7376067f
Mike Salisbury <michael.salisbury87@gmail.com>
2018-02-28 22:07:51 -0800

    style header of game show and structure page layout

--- client/src/components/games/showGame/showGame.jsx
+++ client/src/components/games/showGame/showGame.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./showGame.css";
 
 class ShowGame extends Component {
 
@@ -11,16 +12,24 @@ class ShowGame extends Component {
   }
 
   render() {
+    console.log(this.props.game.players);
     if (this.props.game) {
       return(
         <div className='showGame-wrapper'>
-          <div className='showGame-title'>{this.props.game.title}</div>
-          <div className='showGame-sport'>{this.props.game.sport}</div>
-          <div className='showGame-skill'>{this.props.game.skill}</div>
-          <div className='showGame-location'>{this.props.game.location}</div>
-          <div className='showGame-startDatetime'>{this.props.game.startDatetime}</div>
-          <div className='showGame-endDatetime'>{this.props.game.endDatetime}</div>
-
+          <div className='showGame-header'>
+            <div className='showGame-heroImage' />
+            <div className='showGame-info'>
+              <div className='showGame-title'>{this.props.game.title}</div>
+              <div className='showGame-sport'>{this.props.game.sport}</div>
+              <div className='showGame-numPlayers'>{this.props.game.numPlayers}</div>
+              <div className='showGame-skill'>{this.props.game.skill}</div>
+              <div className='showGame-location'>{this.props.game.location}</div>
+              <div className='showGame-startDatetime'>{this.props.game.startDatetime}</div>
+              <div className='showGame-endDatetime'>{this.props.game.endDatetime}</div>
+            </div>
+          </div>
+          
+          <div className='showGame-locationMap' />
         </div>
       );
     } else {
