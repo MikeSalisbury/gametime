6d2323dbb47eba11babda89c0473574c23c0e901
Mike Salisbury <michael.salisbury87@gmail.com>
2018-03-03 19:18:57 -0800

    style buttons for browseGamesFilter

--- client/src/components/games/browseGames/browseGames.jsx
+++ client/src/components/games/browseGames/browseGames.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowseGamesItem } from './browseGamesItem';
+import './browseGames.css';
 
 class BrowseGames extends Component {
 
@@ -14,19 +15,31 @@ class BrowseGames extends Component {
   render() {
     if(this.props.games){
       const games = Object.values(this.props.games);
-      console.log(games);
       return(
+
         <div className='browseGames-wrapper'>
-          {games.map(game => <BrowseGamesItem
-            key={`game-${game._id}`}
-            title={game.title}
-            sport={game.sport}
-            skill={game.skill}
-            numPlayers={game.numPlayers}
-            location={game.location}
-            startDatetime={game.startDatetime}
-            endDatetime={game.endDatetime} />
-          )}
+          <div className='browseGames-left'>
+            <div className='browseGames-map'>Map</div>
+            <div className='browseGames-filter'>
+              <button className='filterButton'>Basketball</button>
+              <button className='filterButton'>Football</button>
+              <button className='filterButton'>Cricket</button>
+              <button className='filterButton'>Soccer</button>
+              <button className='filterButton'>Baseball</button>
+            </div>
+          </div>
+          <div className='browseGames-right'>
+            {games.map(game => <BrowseGamesItem
+              key={`game-${game._id}`}
+              title={game.title}
+              sport={game.sport}
+              skill={game.skill}
+              numPlayers={game.numPlayers}
+              location={game.location}
+              startDatetime={game.startDatetime}
+              endDatetime={game.endDatetime} />
+            )}
+          </div>
         </div>
       );
     } else {
