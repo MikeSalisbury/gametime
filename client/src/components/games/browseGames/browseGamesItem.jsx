0de3b07a2a2a97e92127b9668a901c8c5d20d3d6
Mike Salisbury <michael.salisbury87@gmail.com>
2018-03-08 10:27:17 -0800

    style browse items component

--- client/src/components/games/browseGames/browseGamesItem.jsx
+++ client/src/components/games/browseGames/browseGamesItem.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 export function BrowseGamesItem({gameImage, title, sport, skill, numPlayers,
 players, location, startDatetime, endDatetime}) {
   return(
-    <div className='browseGameItem-info'>
-      <img src={`${gameImage}`} />
-      <h1>{title}</h1>
-      <h1>{sport}</h1>
-      <h1>{skill}</h1>
-      <h1>{numPlayers}</h1>
-      <h1>{location}</h1>
-      <h1>{startDatetime}</h1>
-      <h1>{endDatetime}</h1>
+    <div className='browseGameItem-container'>
+      <img src={`${gameImage}`} className='browseGameItem-image'/>
+      <div className='browseGameItem-info'>
+        <h1>{title}</h1>
+        <h1>{sport}</h1>
+        <h1>{skill}</h1>
+        <h1>{numPlayers}</h1>
+        <h1>{location}</h1>
+        <h1>{startDatetime}</h1>
+        <h1>{endDatetime}</h1>
+      </div>
     </div>
   );
 }
