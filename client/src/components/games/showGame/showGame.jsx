ed91f4b03ecb56d0501f9ca4ac16e798c6d3b131
Mike Salisbury <michael.salisbury87@gmail.com>
2018-04-01 23:13:19 -0700

    update details for showGame Component

--- client/src/components/games/showGame/showGame.jsx
+++ client/src/components/games/showGame/showGame.jsx
@@ -14,7 +14,7 @@ class ShowGame extends Component {
   render() {
     if (this.props.game) {
       let game = this.props.game[this.props.match.params.gameId];
-      const { title, sport, players, numPlayers, skill, location,
+      const { title, gameImage, sport, players, numPlayers, skill, location,
          startDatetime, endDatetime } = game;
       let gamePlayers = players.map(player => player.firstName + ' ' + player.lastName);
 
@@ -23,12 +23,12 @@ class ShowGame extends Component {
       return(
         <div className='showGame-wrapper'>
           <div className='showGame-header'>
-            <div className='showGame-heroImage'>GAME IMAGE</div>
+            <div className='showGame-heroImage'><img src={`${gameImage}`} alt='game image'></img></div>
             <div className='showGame-info'>
               <div className='showGame-title'>{title}</div>
               <div className='showGame-sport'>{sport}</div>
-              <div className='showGame-numPlayers'>{game.players.length}/{numPlayers} players</div>
-              <div className='showGame-skill'>{skill}</div>
+              <div className='showGame-numPlayers'>{game.players.length}/{numPlayers} Players</div>
+              <div className='showGame-skill'>{skill} Level</div>
               <div className='showGame-location'>{location}</div>
               <div className='showGame-startDatetime'>{startDatetime}</div>
               <div className='showGame-endDatetime'>{endDatetime}</div>
