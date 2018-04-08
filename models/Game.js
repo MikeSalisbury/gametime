449aede812f96355e6b7601ba5f03c2e16922423
Mike Salisbury <michael.salisbury87@gmail.com>
2018-04-07 22:56:10 -0700

    fix game model

--- models/Game.js
+++ models/Game.js
@@ -4,16 +4,16 @@ const { Schema } = mongoose;
 
 // players: [{ type: Schema.Types.ObjectId, ref: 'User'}] to store OID as reference to users
 const gameSchema = new Schema({
-  gameManager: [{type: String, required : true}],
-  gameImage: [{type: String, required : true}],
-  title: [{type: String, required : true}],
-  sport: [{type: String, required : true}],
-  numPlayers: [{type: Number, required : true}],
-  skill: [{type: String, required : true}],
-  players: [{type: Array, required : true}],
-  location: [{type: String, required : true}],
-  startDatetime: [{type: Date, required : true}],
-  endDatetime: [{type: Date, required : true}],
+  gameManager: {type: String, required : true},
+  gameImage: {type: String, required : true},
+  title: {type: String, required : true},
+  sport: {type: String, required : true},
+  numPlayers: {type: Number, required : true},
+  skill: {type: String, required : true},
+  players: {type: Array, required : true},
+  location: {type: String, required : true},
+  startDatetime: {type: Date, required : true},
+  endDatetime: {type: Date, required : true},
   lat: String,
   lng: String
 },
